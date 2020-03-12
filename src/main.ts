a5cd34a524a4f5b5a1e0ba6e1d832557e2b19cd9
acnolan <acnolan@wpi.edu>
2020-03-12 15:25:12 -0400

    Reading in CSV files test
    So far it just reads in the data points from a csv. It should be noted this is only temporary. It's just in javascript, I was getting errors in typescript i'll try to fix that next

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,32 @@
 function main(): void {
-  const testMessage = document.createElement("p");
-  testMessage.innerText = "Successfully ran compiled TypeScript";
-  document.body.appendChild(testMessage);
+  const csvMessage = document.createElement("p");
+  csvMessage.innerText = "Select the Sample Data File";
+  document.body.appendChild(csvMessage);
+
+  const csvInput = document.createElement("input");
+  csvInput.type = "file";
+  csvInput.id = "csv";
+  csvInput.accept = ".csv";
+  document.body.appendChild(csvInput);
+
+  const outputArea = document.createElement("output");
+  outputArea.id = "out";
+  outputArea.innerHTML = "Results will go here";
+  document.body.appendChild(outputArea);
 }
 
 window.onload = main;
+
+const dataPoints = [];
+
+const fileInput = document.getElementById("csv");
+
+const readFile = function() {
+  const reader = new FileReader();
+  reader.onload = function() {
+    document.getElementById("out").innerHTML = reader.result;
+  };
+  reader.readAsBinaryString(fileInput.files[0]);
+};
+
+fileInput?.addEventListener("change", readFile);
